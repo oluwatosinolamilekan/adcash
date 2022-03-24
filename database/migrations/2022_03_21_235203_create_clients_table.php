b23696d66226814000e1967d36cbfffaa06ca7e3
oluwatosinolamilekan <oluwatosinolamilekan@gmail.com>
2022-03-24 16:05:32 +0100

    remove invested fund column

--- database/migrations/2022_03_21_235203_create_clients_table.php
+++ database/migrations/2022_03_21_235203_create_clients_table.php
@@ -16,7 +16,6 @@ class CreateClientsTable extends Migration
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('investment_fund')->default(1000);
             $table->timestamps();
         });
     }
